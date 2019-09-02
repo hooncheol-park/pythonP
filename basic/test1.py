88165088f383bf6a645f213e80f6c3144a3ff648
hooncheol-park <honlin72@gmail.com>
2019-09-02 12:51:53 +0900

    1일차테스트

--- basic/test1.py
+++ basic/test1.py
@@ -1,1 +1,25 @@
-print("hello")
+a=4
+b=3
+print(a+b)
+print("A"+"B")
+print(a-b)
+print("AB"*2)
+print(a*b)
+print(a**b)
+print(a/b)
+print(a%b)
+print(a//b)
+print("=" * 50)
+print("My Program")
+print("=" * 50)
+print("Life is too short\nYou need python")
+
+print("""Life is too short
+You need python""")
+
+head="Pthon "
+
+tail="is fun"
+print(head+str(a)+" "+tail)
+
+print(type(head))
